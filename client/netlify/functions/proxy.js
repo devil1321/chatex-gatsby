9caf1d389656e6e3170821e40ea411e0b30ca322
devil1321 <s.dominik1321@gmail.com>
2023-08-02 16:14:54 +0200

    mobile portrait finished only mobile landscape to end

--- client/netlify/functions/proxy.js
+++ client/netlify/functions/proxy.js
@@ -6,7 +6,7 @@ exports.handler = async (event, context) => {
 
   try {
     // Your backend API base URL
-    const backendBaseUrl = 'https://chatex-14m2.onrender.com';
+    const backendBaseUrl = 'http://chatex-14m2.onrender.com';
     const customPath = path.replace('/.netlify/functions/proxy','')
     
     const requestConfig = {
@@ -18,7 +18,7 @@ exports.handler = async (event, context) => {
     };
 
     const response = await axios.request(requestConfig);
-    
+
     const { data, status, headers } = response;
 
     return {
