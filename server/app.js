a55470e0f75219c5e8e05c9a1f8eed263ae433d6
devil1321 <s.dominik1321@gmail.com>
2023-08-03 15:27:38 +0200

    mobile portrait finished only mobile landscape to end

--- server/app.js
+++ server/app.js
@@ -99,8 +99,7 @@ passport.use(new GoogleStrategy({
         }else{
             const userObj = JSON.parse(data)
             userObj.token = user.token
-            console.log('user',user,'userob',userObj)
-            done(null,userObj);
+            done(null,{user:userObj});
         }
     }))
   });
