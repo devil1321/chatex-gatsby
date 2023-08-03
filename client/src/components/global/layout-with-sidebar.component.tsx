721cd1d9bf7ff5fa0648e109672f5a41cdb1b1c7
devil1321 <s.dominik1321@gmail.com>
2023-08-03 16:53:19 +0200

    mobile portrait finished only mobile landscape to end

--- client/src/components/global/layout-with-sidebar.component.tsx
+++ client/src/components/global/layout-with-sidebar.component.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Seo from './seo.component';
 import Sidebar from './sidebar.component';
 import Nav from './nav.component';
 import Footer from './footer.component';
 import '../../theme/styles.scss'
+
+import { navigate } from 'gatsby';
+import * as ApiActions from '../..//controller/actions-creators/api.actions-creators'
+import { bindActionCreators } from 'redux';
+import { useDispatch,useSelector } from 'react-redux';
+import { State } from '../../controller/reducers'
 
 interface LayoutProps{
     title:string;
@@ -12,6 +18,21 @@ interface LayoutProps{
 }
 
 const LayoutWithSidebar:React.FC<LayoutProps> = ({title,className,children}) => {
+
+  const dispatch = useDispatch()
+  const apiActions = bindActionCreators(ApiActions,dispatch)
+  const { user,access_token } = useSelector((state:State) => state.api)
+  
+  useEffect(()=>{
+    if(typeof window !== undefined){
+        if(!user && !access_token){
+            apiActions.isLogged()
+        }else{
+          navigate('/login')
+        }
+    }
+  },[user])
+
   
   return (
     <div className={className}>
--- client/src/components/global/layout.component.tsx
+++ client/src/components/global/layout.component.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Seo from './seo.component';
 import Nav from './nav.component';
 import Footer from './footer.component';
 import '../../theme/styles.scss'
+
+import { navigate } from 'gatsby';
+import * as ApiActions from '../..//controller/actions-creators/api.actions-creators'
+import { bindActionCreators } from 'redux';
+import { useDispatch,useSelector } from 'react-redux';
+import { State } from '../../controller/reducers'
 
 interface LayoutProps{
     title:string;
@@ -11,6 +17,19 @@ interface LayoutProps{
 }
 
 const Layout:React.FC<LayoutProps> = ({title,className,children}) => {
+  const dispatch = useDispatch()
+  const apiActions = bindActionCreators(ApiActions,dispatch)
+  const { user,access_token } = useSelector((state:State) => state.api)
+  
+  useEffect(()=>{
+    if(typeof window !== undefined){
+        if(!user && !access_token){
+            apiActions.isLogged()
+        }else{
+          navigate('/login')
+        }
+    }
+  },[user])
 
   return (
     <div className={className}>
