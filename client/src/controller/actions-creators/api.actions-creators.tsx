14834e0da5e30645b8b5d85a30bb5ff88998f47b
devil1321 <s.dominik1321@gmail.com>
2023-08-03 11:23:22 +0200

    mobile portrait finished only mobile landscape to end

--- client/src/controller/actions-creators/api.actions-creators.tsx
+++ client/src/controller/actions-creators/api.actions-creators.tsx
@@ -8,7 +8,7 @@ import { access } from 'fs';
 
 const instance = axios.create({
     // Set the base URL of your back-end (Express server) running on port 3000
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://chatex-14m2.onrender.com',
   
     // Set the proxy configuration to point to your back-end
     // Enable sending credentials (e.g., cookies) to the back-end
