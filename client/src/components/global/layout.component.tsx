e828a5e6c584451a9468f97c2e7a4bec01b49e1d
devil1321 <s.dominik1321@gmail.com>
2023-08-03 14:39:20 +0200

    mobile portrait finished only mobile landscape to end

--- client/src/components/global/layout.component.tsx
+++ client/src/components/global/layout.component.tsx
@@ -1,12 +1,9 @@
-import React,{useEffect} from 'react'
+import React from 'react'
 import Seo from './seo.component';
 import Nav from './nav.component';
 import Footer from './footer.component';
 import '../../theme/styles.scss'
-import { bindActionCreators } from 'redux';
-import { useDispatch,useSelector } from 'react-redux';
-import * as ApiActions from '../../controller/actions-creators/api.actions-creators'
-import { State } from '../../controller/reducers';
+
 interface LayoutProps{
     title:string;
     className:string;
@@ -15,21 +12,6 @@ interface LayoutProps{
 
 const Layout:React.FC<LayoutProps> = ({title,className,children}) => {
 
-  const dispatch = useDispatch()
-  const apiActions = bindActionCreators(ApiActions,dispatch)
-  const { user,access_token } = useSelector((state:State) => state.api)
-
-  useEffect(()=>{
-    if(typeof window !== undefined){
-      if(!access_token){
-        const token = localStorage.getItem('access_token') as string
-        apiActions.handleToken(token)
-      }
-      if(access_token){
-        apiActions.isLogged()
-      }
-    }
-  },[access_token])
   return (
     <div className={className}>
         <Seo title={title} meta={[]}/>
