84a6a52b0ea1c0ccf538d5a5af77fa8cb26b7946
devil1321 <s.dominik1321@gmail.com>
2023-08-04 11:06:53 +0200

    protected routes to do and fix delay from backend

--- client/src/components/global/layout-with-sidebar.component.tsx
+++ client/src/components/global/layout-with-sidebar.component.tsx
@@ -5,6 +5,11 @@ import Nav from './nav.component';
 import Footer from './footer.component';
 import '../../theme/styles.scss'
 
+import { useSelector,useDispatch} from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { State } from '../../controller/reducers';
+import * as ApiActions from '../../controller/actions-creators/api.actions-creators'
+import { navigate } from 'gatsby';
 
 
 interface LayoutProps{
@@ -15,8 +20,29 @@ interface LayoutProps{
 
 const LayoutWithSidebar:React.FC<LayoutProps> = ({title,className,children}) => {
 
+  const [isLoad,setIsLoad] = useState<boolean>(false)
 
+  const dispatch = useDispatch()
+  const apiActions = bindActionCreators(ApiActions,dispatch)
+  const { user } = useSelector((state:State) => state.api)
   
+  const handleRedirect = () =>{
+    if(isLoad && !user){
+      navigate('/login')
+    }
+  }
+
+  useEffect(()=>{
+    apiActions.isLogged()
+    setTimeout(() => {
+      setIsLoad(true)
+    }, 1000);
+  },[])
+
+  useEffect(()=>{
+    handleRedirect()
+  },[isLoad,user])
+
   return (
     <div className={className}>
         <Seo title={title} meta={[]}/>
--- client/src/components/global/layout.component.tsx
+++ client/src/components/global/layout.component.tsx
@@ -4,6 +4,12 @@ import Nav from './nav.component';
 import Footer from './footer.component';
 import '../../theme/styles.scss'
 
+import { useSelector,useDispatch} from 'react-redux'
+import { bindActionCreators } from 'redux';
+import { State } from '../../controller/reducers';
+import * as ApiActions from '../../controller/actions-creators/api.actions-creators'
+import { navigate } from 'gatsby';
+
 
 interface LayoutProps{
     title:string;
@@ -13,6 +19,28 @@ interface LayoutProps{
 
 const Layout:React.FC<LayoutProps> = ({title,className,children}) => {
 
+  const [isLoad,setIsLoad] = useState<boolean>(false)
+
+  const dispatch = useDispatch()
+  const apiActions = bindActionCreators(ApiActions,dispatch)
+  const { user } = useSelector((state:State) => state.api)
+  
+  const handleRedirect = () =>{
+    if(isLoad && !user){
+      navigate('/login')
+    }
+  }
+
+  useEffect(()=>{
+    apiActions.isLogged()
+    setTimeout(() => {
+      setIsLoad(true)
+    }, 1000);
+  },[])
+
+  useEffect(()=>{
+    handleRedirect()
+  },[isLoad,user])
   return (
     <div className={className}>
         <Seo title={title} meta={[]}/>
