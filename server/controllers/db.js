6372d719cf38c7ff7533bf5ed3729cce15443214
devil1321 <70096468+devil1321@users.noreply.github.com>
2024-01-24 03:20:48 +0100

    Update db.js

--- server/controllers/db.js
+++ server/controllers/db.js
@@ -3,7 +3,7 @@ const Redis = require("ioredis");
 
 const redisClient = new Redis({
     port: 18454,
-    host: 'redis-18454.c270.us-east-1-3.ec2.cloud.redislabs.com',
+    host: 'redis-16421.c274.us-east-1-3.ec2.cloud.redislabs.com:16421',
     username:'admin',
     password: 'Admin267#$',
     db: 0, // Defaults to 0
